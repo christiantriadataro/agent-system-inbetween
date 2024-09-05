9f0cfe6bace8e6622b2e3da0acf9d3cea8a2425a
Christian Dataro <christiantriadataro@gmail.com>
2024-09-05 15:40:58 +0800

    SelectLabel Fixed

--- components/SelectwithLabel.tsx
+++ components/SelectwithLabel.tsx
@@ -1,15 +1,14 @@
 import {Label} from "@/components/ui/label";
-import {ChangeEvent, useState} from "react";
+import React, {ChangeEvent, useState} from "react";
 
 interface SelectwithLabelProps {
     label: string,
     values: { name: string, value: string }[],
-    handleDataChange: (event: ChangeEvent<HTMLInputElement>) => void,
+    handleDataChange: (event: React.ChangeEvent<HTMLSelectElement>) => void,
     value?: string,
-    text?: string
 }
 
-const SelectwithLabel = ({values, label, handleDataChange, value, text = "text"}: SelectwithLabelProps) => {
+const SelectwithLabel = ({values, label, handleDataChange, value}: SelectwithLabelProps) => {
     const [showLabel, setShowLabel] = useState(false);
     const [placeholder, setPlaceholder] = useState(label);
 
@@ -23,7 +22,7 @@ const SelectwithLabel = ({values, label, handleDataChange, value, text = "text"}
         setPlaceholder(`${label}:`)
     }
 
-    const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
         handleDataChange(event);
         setShowLabel(false);
     }
@@ -33,13 +32,12 @@ const SelectwithLabel = ({values, label, handleDataChange, value, text = "text"}
         <div className="flex flex-col gap-1 w-full mt-1.5">
             <Label
                 className={`text-[11px] transition duration-300 relative ${showLabel ? "opacity-100" : "translate-y-12 opacity-0"}`}>{label}: </Label>
-            <select type={text}
-                    placeholder={`${placeholder}`}
-                    onFocus={handleFocus}
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={value}
-                    className="w-full text-[10px] leading-3 font-semibold h-9 bg-white text-[#0E0E0E] pl-4 border-2 rounded-md border-another"
+            <select
+                onFocus={handleFocus}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={value}
+                className="w-full text-[10px] leading-3 font-semibold h-9 bg-white text-[#0E0E0E] pl-4 border-2 rounded-md border-another"
             >
                 {values.map((item, index) => (
                     <option key={index} value={item.value}>{item.name}</option>
